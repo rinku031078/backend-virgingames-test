0390c1d2954ab9f7face470c03a38072d9b0582e
rinku031078 <anandtri78@gmail.com>
2022-01-21 20:10:34 +0000

    virgingames backend test

---
+++ src/main/java/com/virgingames/constants/EndPoints.java
@@ -0,0 +1,11 @@
+package com.virgingames.constants;
+
+public class EndPoints {
+
+
+    public static final String END_POINT = "/GetBingoLobbyFeed.do";
+    /**
+     * This is Endpoints of Authentication api
+     */
+
+}
---
+++ src/main/java/com/virgingames/constants/Path.java
@@ -0,0 +1,8 @@
+package com.virgingames.constants;
+
+public class Path {
+
+    public static final String PATH = "/bingo";
+
+
+}
